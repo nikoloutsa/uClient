b9a744d3ba47dd7e63d56c230d552dbba1028217
Vasileios Keramidas <keramidasceid@gmail.com>
2012-07-21 21:40:06 +0300

    Zxing Support

--- src/eu/uberdust/uClient/HomeActivity.java
+++ src/eu/uberdust/uClient/HomeActivity.java
@@ -1,9 +1,15 @@
 package eu.uberdust.uClient;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 
 public class HomeActivity extends SherlockActivity {
@@ -12,6 +18,15 @@ public class HomeActivity extends SherlockActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
+        final Button button = (Button) findViewById(R.id.scan_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	IntentIntegrator integrator = new IntentIntegrator(HomeActivity.this);
+            	integrator.initiateScan(IntentIntegrator.QR_CODE_TYPES);
+
+            }
+        });
     }
 
     @Override
@@ -20,5 +35,17 @@ public class HomeActivity extends SherlockActivity {
         return true;
     }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (result != null) {
+          String contents = result.getContents();
+          if (contents != null) {
+            Log.d("SCAN",contents);
+          } else {
+        	  Log.d("SCAN","FAIL");
+          }
+        }
+      }
+    
     
 }
