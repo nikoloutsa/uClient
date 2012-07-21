9978a2e05976c2afa872e2c4a1200105d9aee598
Vasileios Keramidas <keramidasceid@gmail.com>
2012-07-21 13:56:27 +0300

    Create Project

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ uClient
 =======
 
 Uberdust micro-client for Android
+
+Developed by:
+
+* Keramidas Bill (keramidasceid@gmail.com)
+* Nikoloutsakos Nikos (nikoloutsa@ceid.upatras.gr)
+* Prevezanos John (prevezan@ceid.upatras.gr)
---
+++ src/eu/uberdust/uClient/HomeActivity.java
@@ -0,0 +1,24 @@
+package eu.uberdust.uClient;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_home, menu);
+        return true;
+    }
+
+    
+}
