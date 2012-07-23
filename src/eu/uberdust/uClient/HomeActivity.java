88c70cad236ae7c971c792ecd4292dd5cb8baf6b
Vasileios Keramidas <keramidasceid@gmail.com>
2012-07-23 20:23:09 +0300

    addded ActivityAbout

--- src/eu/uberdust/uClient/HomeActivity.java
+++ src/eu/uberdust/uClient/HomeActivity.java
@@ -1,9 +1,13 @@
 package eu.uberdust.uClient;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+import eu.uberdust.uClient.R.id;
 
 
 public class HomeActivity extends SherlockActivity {
@@ -18,6 +22,19 @@ public class HomeActivity extends SherlockActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
     	getSupportMenuInflater().inflate(R.menu.activity_home, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case id.menu_home:
+                // app icon in action bar clicked; go home
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     
