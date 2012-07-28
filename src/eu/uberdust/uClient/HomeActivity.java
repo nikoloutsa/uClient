728dbf24b598fa4e464bb25c98d7bd9af1a2a82c
Vasileios Keramidas <keramidasceid@gmail.com>
2012-07-28 19:56:01 +0300

    removed ABS

--- src/eu/uberdust/uClient/HomeActivity.java
+++ src/eu/uberdust/uClient/HomeActivity.java
@@ -1,24 +1,19 @@
 package eu.uberdust.uClient;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
-import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuItem;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 import eu.uberdust.model.Capability;
 import eu.uberdust.model.Room;
-import eu.uberdust.uClient.R.id;
 
 
 
-public class HomeActivity extends SherlockActivity {
+public class HomeActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,10 +26,12 @@ public class HomeActivity extends SherlockActivity {
             	Capability capa3 = new Capability();
             	capa1.setName("Temperature");
             	capa1.setValue("10");
+            	capa1.setType(0);
             	capa2.setName("Light");
             	capa2.setValue("11");
+            	capa2.setType(0);
             	capa3.setName("AC");
-            	capa3.setValue("12");
+            	capa3.setType(1);
             	room1.appendCapability(capa1);
             	room1.appendCapability(capa2);
             	room1.appendCapability(capa3);
@@ -47,24 +44,6 @@ public class HomeActivity extends SherlockActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	getSupportMenuInflater().inflate(R.menu.activity_home, menu);
-        return true;
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case id.menu_home:
-                // app icon in action bar clicked; go home
-                Intent intent = new Intent(this, AboutActivity.class);
-                startActivity(intent);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
