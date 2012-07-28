69a5e50913a8d6548ce14f8859d33d1c4ceeaa84
Vasileios Keramidas <keramidasceid@gmail.com>
2012-07-28 18:22:22 +0300

    fragment working

--- src/eu/uberdust/uClient/RoomActivity.java
+++ src/eu/uberdust/uClient/RoomActivity.java
@@ -1,32 +1,62 @@
 package eu.uberdust.uClient;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import eu.uberdust.model.Room;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 
-public class RoomActivity extends Activity {
+import com.viewpagerindicator.PageIndicator;
+import com.viewpagerindicator.TabPageIndicator;
+
+import eu.uberdust.myfragment.TestFragment;
+import eu.uberdust.myfragment.TestFragmentAdapter;
+
+public class RoomActivity extends FragmentActivity {
+	
+	private static final String[] CONTENT = new String[] { "Capabilities", "Commands" };
+    TestFragmentAdapter mAdapter;
+    ViewPager mPager;
+    PageIndicator mIndicator;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
         
-        Intent i = getIntent();
-        Room room = (Room)i.getSerializableExtra("MyRoom");
-        Log.d("PASS",room.getCapability(0).getName());
-        Log.d("PASS",room.getCapability(0).getValue());
-        Log.d("PASS",room.getCapability(1).getName());
-        Log.d("PASS",room.getCapability(1).getValue());
-        Log.d("PASS",room.getCapability(2).getName());
-        Log.d("PASS",room.getCapability(2).getValue());
-    }
+        /*Intent i = getIntent();
+        Room room = (Room)i.getSerializableExtra("MyRoom");*/
+        
+        mAdapter = new GoogleMusicAdapter(getSupportFragmentManager());
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_room, menu);
-        return true;
+        mPager = (ViewPager)findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+        mIndicator = (TabPageIndicator)findViewById(R.id.indicator);
+        mIndicator.setViewPager(mPager);
+        
     }
+    
+  
+    class GoogleMusicAdapter extends TestFragmentAdapter {
+        public GoogleMusicAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return TestFragment.newInstance(RoomActivity.CONTENT[position % RoomActivity.CONTENT.length]);
+        }
+
+        @Override
+        public int getCount() {
+            return RoomActivity.CONTENT.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return RoomActivity.CONTENT[position % RoomActivity.CONTENT.length].toUpperCase();
+        }
+    }
+    
 }
